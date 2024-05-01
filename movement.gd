0cb540690a097432b48dffa13b99a43d52bb0b43
David Lynch <davite3@protonmail.com>
2024-05-01 01:30:46 +0100

    structuring behaviours

--- movement.gd
+++ movement.gd
@@ -1,10 +1,30 @@
 extends CharacterBody3D
 
-const SPEED = 5
+const SPEED = 1
 @export var forward: bool = true
+@export var closest: bool = true
+
+var movements: Array[Callable] = []
+
+
+func _closest_flower() -> Vector3:
+	return Vector3(0, 0, 0)
+
+
+func _forward() -> Vector3:
+	return Vector3(0, 0, SPEED)
+
+
+func _init():
+	if forward:
+		movements.append(_forward)
+	if closest:
+		movements.append(_closest_flower)
 
 
 func _physics_process(delta):
-	if forward:
-		velocity = Vector3(0, 0, SPEED)
+	var vel: Vector3 = Vector3(0, 0, 0)
+	for i in range(movements.size()):
+		vel += movements[i].call()
+	velocity = vel
 	move_and_slide()
