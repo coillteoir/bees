671951d227744500b0d1c857e9fedf3192241c57
James <jamesclarkedublin@gmail.com>
2024-05-01 17:25:04 +0100

    Arrive behaviour added

--- bee.gd
+++ bee.gd
@@ -1,30 +1,83 @@
 extends CharacterBody3D
 
-const SPEED = 1
-@export var forward: bool = true
-@export var closest: bool = true
+#Behaviours
+@export var forward: bool = false
+@export var closest: bool = false
+@export var arrive: bool = true
 
 var movements: Array[Callable] = []
 
+#Boid properties
+const MAX_SPEED: float = 10.0
+const MAX_FORCE: float = 10.0
+
+var mass = 1
+var vel: Vector3 = Vector3.ZERO
+var force: Vector3 = Vector3.ZERO
+var acceleration: Vector3 = Vector3.ZERO
+var speed: float
+
+var target = null
 
 func _closest_flower() -> Vector3:
 	return Vector3(0, 0, 0)
 
-
 func _forward() -> Vector3:
-	return Vector3(0, 0, SPEED)
+	return Vector3(0, 0, MAX_SPEED)
 
-
+func _arrive() -> Vector3:
+	const SLOWING_DISTANCE = 50
+	var toTarget = target.global_transform.origin - global_transform.origin #get vector to target
+	var distToTarget = toTarget.length() #get distance to target
+	
+	if distToTarget < 2: #if distance is less than 2, stop
+		return Vector3.ZERO
+		
+	var rampedSpeed = (distToTarget / SLOWING_DISTANCE) * MAX_SPEED #sets speed based on ratio between dist and slowingDistance, scaled to max_speed
+	
+	var limitedSpeed = min(MAX_SPEED, rampedSpeed) #Limit speed
+	var desiredVel = (toTarget * limitedSpeed) / distToTarget #desired velcity vector to get to target
+	return desiredVel - vel #returns steering force		
+		
 func _init():
 	if forward:
 		movements.append(_forward)
 	if closest:
 		movements.append(_closest_flower)
-
-
-func _physics_process(delta):
-	var vel: Vector3 = Vector3(0, 0, 0)
+	if arrive:
+		movements.append(_arrive)
+		
+func _ready():
+	target = $"../testTarget"
+	
+func calculate() -> Vector3:
+	var forceAccumulator = Vector3.ZERO
+	
 	for i in range(movements.size()):
-		vel += movements[i].call()
-	velocity = vel
-	move_and_slide()
+		forceAccumulator += movements[i].call()
+	
+	#limit force
+	if forceAccumulator.length() > MAX_FORCE:
+		forceAccumulator = force.limit_length(MAX_FORCE)
+	
+	return forceAccumulator
+
+func _physics_process(delta):	
+	var newForce = calculate()
+
+	force = lerp(force, newForce, delta)
+		
+	acceleration = force/mass
+	
+	#multiply acceleration by delta (time since last frame) to account for inconsistent framerate
+	vel += acceleration * delta 
+	
+	speed = vel.length()
+	
+	if speed > 0:
+		vel = vel.limit_length(MAX_SPEED)
+		
+		set_velocity(vel)
+		move_and_slide()
+	
+	
