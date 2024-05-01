56c9462388c0f291fc50f8fe6d86003c6efcff99
James <jamesclarkedublin@gmail.com>
2024-05-01 19:26:41 +0100

    Wander partially implemented

--- bee.gd
+++ bee.gd
@@ -3,12 +3,13 @@ extends CharacterBody3D
 #Behaviours
 @export var forward: bool = false
 @export var closest: bool = false
-@export var arrive: bool = true
+@export var arrive: bool = false
+@export var noiseWander: bool = true
 
 var movements: Array[Callable] = []
 
 #Boid properties
-const MAX_SPEED: float = 10.0
+const MAX_SPEED: float = 1.0
 const MAX_FORCE: float = 10.0
 
 var mass = 1
@@ -18,6 +19,19 @@ var acceleration: Vector3 = Vector3.ZERO
 var speed: float
 
 var target = null
+
+#Noise Wander Vars
+enum Axis { Horizontal, Vertical}
+
+var wanderTarget:Vector3
+var theta = 50
+var amplitude = 200
+var distance = 5
+var noise:FastNoiseLite = FastNoiseLite.new()
+var axis = Axis.Horizontal
+var frequency = 0.3
+var radius = 10.0
+var world_target:Vector3
 
 func _closest_flower() -> Vector3:
 	return Vector3(0, 0, 0)
@@ -38,6 +52,41 @@ func _arrive() -> Vector3:
 	var limitedSpeed = min(MAX_SPEED, rampedSpeed) #Limit speed
 	var desiredVel = (toTarget * limitedSpeed) / distToTarget #desired velcity vector to get to target
 	return desiredVel - vel #returns steering force		
+	
+func _noiseWander() -> Vector3:
+	var n  = noise.get_noise_1d(theta)
+	print(n)
+	
+	var angle = deg_to_rad(n * amplitude)
+	
+	var delta = get_process_delta_time()
+
+	var rot = global_transform.basis.get_euler()
+	
+	rot.x = 0
+	if axis == Axis.Horizontal:
+		wanderTarget.x = sin(angle)
+		wanderTarget.y = 0
+		wanderTarget.z =  cos(angle)		
+		rot.z = 0
+	else:
+		wanderTarget.x = 0
+		wanderTarget.y = sin(angle)
+		wanderTarget.z = cos(angle)		
+		
+	wanderTarget *= radius
+
+	var local_target = wanderTarget + (Vector3.BACK * distance)
+	
+	var projected = Basis.from_euler(rot)
+	
+	world_target = global_transform.origin + (projected * local_target)	
+	theta += frequency * delta * PI * 2.0
+	
+	var toTarget = world_target - global_transform.origin
+	toTarget = toTarget.normalized()
+	var desired = toTarget * MAX_SPEED
+	return desired - vel
 		
 func _init():
 	if forward:
@@ -46,6 +95,8 @@ func _init():
 		movements.append(_closest_flower)
 	if arrive:
 		movements.append(_arrive)
+	if noiseWander:
+		movements.append(_noiseWander)
 		
 func _ready():
 	target = $"../testTarget"
