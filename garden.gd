cae776cabc8822f6212a5ed4e48eb5acd0511b85
MossDog <lukejohnmosshughes@gmail.com>
2024-05-16 00:06:36 +0100

    New method of patch & flower spawning implemented. Patch area now dynamically increased when a certain number of failures are reached. This entirely avoids crashes due to insufficient area. The higher the number of allowed failures, the more ideally the patch locations will be set. This is an efficiency vs effectiveness tradeoff. A limit of 20 failures produced nice results with little time tradoff, when tested with various patch numbers

--- garden.gd
+++ garden.gd
@@ -1,28 +1,34 @@
 extends Node3D
 
-@export var patch_count: int = 1
+const patch_marker: PackedScene = preload("res://patch_marker.tscn")
+const flower_template: PackedScene = preload("res://flower.tscn")
 
-#Defines the maximum amount of 
-var max_flowers_per_patch: int = 7
-
-# Defines the minimum distance between flowers
-var flower_min_distance: int = 6
-
-# Defines the area of patches and the distance between them
-var flower_area: float = sqrt(max_flowers_per_patch * flower_min_distance) * 3
-
-var hive_buffer: int = 40
-
-# Defines the area that patches are enclosed in
-var patch_area: int = (flower_area * 2 ) * patch_count
+# Defines the minimum distance between flowers.
+const flower_min_distance: int = 7
+# Defines the maximum amount of flowers per patch.
+const max_flowers_per_patch: int = 10
+# Defines the radius around the patch center flowers may spawn.
+const flower_area: int = 12
+# Defines the minimum distance between patches.
+const patch_min_distance: int = 35
+# Defines the radius around the buffer where no patches will spawn.
+const hive_buffer: int = 20
 
 var patch_flowers: Array = []
 var patch_points: Array = []
 
-var flower_template: PackedScene = preload("res://flower.tscn")
+# Defines the number of patches to spawn.
+@export var patch_count: int = 15
+
+# Defines the max distance from the hive that a patch can be.
+var patch_area: float = hive_buffer + flower_area
 
 
 func _init():
+	#for i in range(15):
+		#var patch_mesh = patch_marker.instantiate()
+		#add_child(patch_mesh)
+		#patch_mesh.global_position.x = i
 	pass
 
 
@@ -47,7 +53,7 @@ func _process(_delta):
 func validate_patch_point(new_point):
 	# Check if the new point is at least min_distance away from existing points
 	for existing_point in patch_points:
-		if new_point.distance_to(existing_point) < flower_min_distance:
+		if new_point.distance_to(existing_point) < patch_min_distance:
 			return false
 	return true
 
@@ -66,38 +72,50 @@ func validate_flower_point(new_point):
 # Flower coordinates are generated within a bounding area "flower_area"
 # Flower coordinates are generated with at least "flower_hive_distance" away from the origin (0,0)
 func generate_flower_patches():
+	var fail_count = 0
 	while patch_points.size() < patch_count:
+		if fail_count == 20:
+			patch_area += 5
+			fail_count = 0
+			print("INCREASING PATCH AREA")
+
 		var patch_point = Vector2(
 			randf_range(-patch_area, patch_area), randf_range(-patch_area, patch_area)
 		)
 
 		# Check if the new point is at least min_distance_origin away from the origin (0, 0)
-		if !validate_patch_point(patch_point) and patch_point.length() < patch_area and patch_point.length() > flower_area:
+		if !validate_patch_point(patch_point) or patch_point.length() < hive_buffer:
+			fail_count+=1
 			continue
 
 		patch_points.append(patch_point)
 		print("PATCH POINT")
 		print(patch_point)
 		
+		##Testing patch spawns
+		#var patch_mesh = patch_marker.instantiate()
+		#add_child(patch_mesh)
+		#patch_mesh.global_position.x = patch_point.x
+		#patch_mesh.global_position.z = patch_point.y
+		
 		var num_flowers = randi_range(max_flowers_per_patch/2, max_flowers_per_patch)
-		print("NUM FLOWERS")
-		print(num_flowers)
+		#print("NUM FLOWERS")
+		#print(num_flowers)
 		
 		patch_flowers.append([])
 		
 		while patch_flowers[-1].size() < num_flowers:
 			var flower_point = Vector2(
-				randf_range(patch_points[-1].x - (flower_area/2), patch_points[-1].y + (flower_area/2)),
-				randf_range(patch_points[-1].x - (flower_area/2), patch_points[-1].y + (flower_area/2))
+				randf_range(patch_points[-1].x - (flower_area), patch_points[-1].x + (flower_area)),
+				randf_range(patch_points[-1].y - (flower_area), patch_points[-1].y + (flower_area))
 			)
-			print("FLOWER POINT")
-			print(flower_point)
+			#print("FLOWER POINT")
+			#print(flower_point)
 
 			# Check if the new point is at least min_distance_origin away from the origin (0, 0)
 			if !validate_flower_point(flower_point):
-				print("INVALID")
+				print("FLOWER INVALID")
 				continue
-			print("VALID")
 		
 			var new_flower = flower_template.instantiate()
 			add_child(new_flower)
